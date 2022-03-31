37fc2a870a7c79018a9b57f5e3fa5f3c0f378fa3
deadfire <root@jolicoeur.deadfire.fr>
2022-03-31 14:44:38 +0200

    Credentials Object + Application Layout

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware('isLogin')->name("dashboard");
+Route::get('/', 'App\Http\Controllers\DashboardController@view')->middleware('isLogin')->name("dashboard");
 
 
 Route::prefix('auth')->group(function () {
